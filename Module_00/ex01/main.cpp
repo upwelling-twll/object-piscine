102ec81dbff8da01fe7d17a7a423846de43b7a74
Natalia Magdanova <nmagdano@lab3r6s4.42abudhabi.ae>
2026-05-28 14:40:16 +0400

    version 1

--- Module_00/ex01/main.cpp
+++ Module_00/ex01/main.cpp
@@ -2,26 +2,18 @@
 
 int main()
 {
-    Graph graph(Vector2{5, 5});
-    try
-    {   
+    try 
+    {
+        Graph graph(Vector2{5, 5});
         graph.addPoint(Vector2{0, 0});
         graph.addPoint(Vector2{2, 2});
-        graph.addPoint(Vector2{40, 2});
+        graph.addPoint(Vector2{4, 2});
         graph.addPoint(Vector2{2, 4});
+        graph.draw();
     }
     catch (const char* msg)
     {
         std::cerr << "Error: " << msg << std::endl;
     }
-
-    // graph.addPoint(Vector2{1, 3});
-    // graph.addPoint(Vector2{2, 2});
-    // graph.addPoint(Vector2{3, 4});
-    // graph.addPoint(Vector2{1, 1});
-    // graph.addPoint(Vector2{0, 2});
-    // graph.addPoint(Vector2{2, 1});
-
-    graph.draw();
     return 0;
 }
