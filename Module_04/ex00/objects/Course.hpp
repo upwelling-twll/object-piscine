6edf102074977de438effc7714f6bafa702300e7
Natalia Magdanova <nmagdano@lab3r8s8.42abudhabi.ae>
2026-06-13 18:32:31 +0400

    upd : course

--- Module_04/ex00/objects/Course.hpp
+++ Module_04/ex00/objects/Course.hpp
@@ -7,17 +7,17 @@ class Course
 {
 private:
 
-	std::string _name;
-	Professor* _responsable;
-	std::vector<Student*> _students;
-	int _numberOfClassToGraduate;
-	int _maximumNumberOfStudent;
+	std::string				_name;
+	Professor*				_responsable;
+	std::vector<Student*>	_students;
+	int 					_numberOfClassToGraduate;
+	int						_maximumNumberOfStudent;
 
 public:
 
 	Course(std::string p_name);
-	void assign(Professor* p_professor);
-	void subscribe(Student* p_student);
+	void	assign(Professor* p_professor);
+	void	subscribe(Student* p_student);
 };
 
 std::ostream& operator<<(std::ostream &output_stream, Course& src);
