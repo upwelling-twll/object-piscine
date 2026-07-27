03823fa43912dce8e11bbedaaf7275a6a05f1ce4
Natalia Magdanova <nmagdano@lab2r2s2.42abudhabi.ae>
2026-07-27 15:08:21 +0400

    add important header

--- Module_05/src/main.cpp
+++ Module_05/src/main.cpp
@@ -3,13 +3,31 @@
 
 #include "Debug.hpp"
 
+void print_welcom_header()
+{
+    constexpr std::string_view cyan = "\033[1;36m";
+    constexpr std::string_view yellow = "\033[1;33m";
+    constexpr std::string_view green = "\033[1;32m";
+    constexpr std::string_view reset = "\033[0m";
+
+    std::cout << green << R"(
++=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+
+ ◊ ◊ ◊  "TIMER UL" RAHIM ITEGEZ  ◊ ◊ ◊
++=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+
+)" << reset;
+
+    std::cout << yellow << "            Khaerle Ul !" << green << reset << "\n";
+    std::cout << "+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+\n\n";
+}
+
 int main(int argc, char** argv)
 {
     if (argc != 3)
         return (1);
         // ERROR();
-    std::cout << "hello from Timer Ul " << std::endl;
+    print_welcom_header();
     parseRailwayNetwork(argv[1]);
     parseTrains(argv[2]);
+
     return (0);
 }
--- Module_05/src/parsing/ParseTrains.cpp
+++ Module_05/src/parsing/ParseTrains.cpp
@@ -78,7 +78,7 @@ std::expected<Train, std::string> parseOneTrain(std::string_view line)
         return std::unexpected("Invalid train definition. Departure time: ");
     if (!saveTime(&train, stop, "stop"))
         return std::unexpected("Invalid train definition Stop time: ");
-    //TODO : validate all physical characteresic of train (weight; maxSpeed; acceleration; deceleration) 
+    //TODO : validate all physical characteristics of train (weight; maxSpeed; acceleration; deceleration) 
     return train;
 }
 
